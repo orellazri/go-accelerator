522104c5f3dd0d8f1c6d0c1017f5f01e420cae29
Orel <ryiseld@gmail.com>
2021-09-23 13:23:08 +0300

    Add progress bar for download

--- download.go
+++ download.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
 	"sync"
 )
 
@@ -15,8 +16,6 @@ type Download struct {
 }
 
 func (d Download) DownloadSection(num int, start int, end int) error {
-	fmt.Printf("Downloading section number %d...\n", num)
-
 	req, err := http.NewRequest("GET", d.url, nil)
 	req.Header.Set("User-Agent", "Go Accelerator")
 	req.Header.Set("Range", fmt.Sprintf("bytes=%d-%d", start, end))
@@ -39,6 +38,8 @@ func (d Download) DownloadSection(num int, start int, end int) error {
 		return err
 	}
 	return nil
+	// time.Sleep(1 * time.Second)
+	// return nil
 }
 
 func (d Download) MergeSections() error {
@@ -48,7 +49,6 @@ func (d Download) MergeSections() error {
 	}
 	defer outputFile.Close()
 	for i := 0; i < d.threads; i++ {
-		fmt.Printf("Merging section number %d...\n", i)
 		b, err := ioutil.ReadFile(fmt.Sprintf("section-%d.tmp", i))
 		if err != nil {
 			return err
@@ -95,11 +95,14 @@ func (d Download) Go() error {
 	sectionSize := sizeInBytes / d.threads
 	var sections = make([][2]int, d.threads) // for example: [[0,10], [11, 21], [22, 32], [33, 43]...]
 
+	fmt.Println("Downloading...")
+	var progressWidth = 60
+	var progress = 0
+	fmt.Printf("[>%s] %dMB", strings.Repeat(" ", progressWidth), sizeInBytes/1e+6)
+
 	var wg sync.WaitGroup
-
+	var progressMutex sync.Mutex
 	for i := range sections {
-		i := i
-
 		// Start byte
 		if i == 0 {
 			sections[i][0] = 0
@@ -115,15 +118,24 @@ func (d Download) Go() error {
 		}
 
 		wg.Add(1)
-		go func() {
+		go func(i int) {
+			defer wg.Done()
+
 			d.DownloadSection(i, sections[i][0], sections[i][1])
-			wg.Done()
-		}()
+			progressMutex.Lock()
+			progress++
+			progressWidthRatio := int((float32(progress) / float32(d.threads)) * float32(progressWidth))
+			fmt.Printf("\r[%s>%s] %dMB", strings.Repeat("=", progressWidthRatio), strings.Repeat(" ", progressWidth-progressWidthRatio), sizeInBytes/1e+6)
+			progressMutex.Unlock()
+		}(i)
 	}
 
 	wg.Wait()
 
+	fmt.Print("\n\n")
+
 	// Merge sections
+	fmt.Println("Merging sections...")
 	d.MergeSections()
 
 	return nil
